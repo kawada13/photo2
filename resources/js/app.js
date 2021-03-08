5e57820470dd07301e52aa933afeb904214bc3ba
kawada <vgnsl94@gmail.com>
2021-03-08 23:42:30 +0900

    s

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,15 +4,17 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
+import './bootstrap'
 
 import Vue from 'vue'
 import router from './router'
 import App from './App.vue'
+import store from './store/index'
 
 new Vue({
   el: '#app',
   router, // ルーティングの定義を読み込む
+  store,
   components: { App }, // ルートコンポーネントの使用を宣言する
   template: '<App />' // ルートコンポーネントを描画する
 })
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\LoginController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +17,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
+
+// 会員登録
+Route::post('/register', [RegisterController::class, 'register'])->name('register');
+
+// ログイン
+Route::post('/login', [LoginController::class], 'login')->name('login');
+
+// ログアウト
+Route::post('/logout', [LoginController::class], 'logout')->name('logout');
+
+
