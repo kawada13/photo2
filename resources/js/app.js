7dd7407404f7135b78105220e21f3467ee828092
kawada <vgnsl94@gmail.com>
2021-03-08 14:25:46 +0900

    router設定

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,8 +7,12 @@
 require('./bootstrap');
 
 import Vue from 'vue'
+import router from './router'
+import App from './App.vue'
 
 new Vue({
   el: '#app',
-  template: '<h1>Hello world</h1>'
+  router, // ルーティングの定義を読み込む
+  components: { App }, // ルートコンポーネントの使用を宣言する
+  template: '<App />' // ルートコンポーネントを描画する
 })
