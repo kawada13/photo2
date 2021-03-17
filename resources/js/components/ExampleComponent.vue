5ed7fb3427b45308a18abd60ecfb2a33336ce612
kawada <vgnsl94@gmail.com>
2021-03-17 14:00:01 +0900

    あーカーふど

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -1,23 +1,22 @@
 <template>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Example Component</div>
-
-                    <div class="card-body">
-                        I'm an example component.
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+  <div>
+      <p>こんにちは</p>
+  </div>
 </template>
 
 <script>
-    export default {
-        mounted() {
-            console.log('Component mounted.')
+export default {
+    data() {
+        return {
+            title: ''
         }
-    }
+    },
+}
 </script>
+
+<style scoped>
+div p {
+    background-color: red;
+}
+
+</style>
